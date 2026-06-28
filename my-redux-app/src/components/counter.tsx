e90a82fc4915c64d6b78352cbf33386f31f83521
Atharva <atharvakumbhar631@gmail.com>
2026-06-28 19:09:32 +0530

    redux

---
+++ my-redux-app/src/components/counter.tsx
@@ -0,0 +1,10 @@
+import type { formatProdErrorMessage } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } form 
+"../store/counterSlice";
+
+import { decrement, increment, selectCount} from "../store/counterSlice";
+
+
+export default = useDispatch={};
+
+    const count = useSelector(selector)
